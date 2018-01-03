3cfa60df27b2b92fd75658d205eb1a8ffc1979c4
chandra sekhar avula <cavula@innominds.com>
2018-01-03 17:53:09 +0530

    dfd

--- cluster-quorum-xml/src/main/java/com/hazlecast/samples/ClusterQuorum.java
+++ cluster-quorum-xml/src/main/java/com/hazlecast/samples/ClusterQuorum.java
@@ -1,6 +1,9 @@
 package com.hazlecast.samples;
+import java.util.concurrent.BlockingQueue;
+
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceNotActiveException;
 import com.hazelcast.core.ILock;
 import com.hazelcast.core.IQueue;
 import com.hazelcast.quorum.QuorumException;
@@ -10,20 +13,31 @@ public class ClusterQuorum {
     public static void main(String[] args) throws Exception {
         final HazelcastInstance instance1 = Hazelcast.newHazelcastInstance();
         final HazelcastInstance instance2 = Hazelcast.newHazelcastInstance();
-        final IQueue<String> queue = instance1.getQueue("queueWithQuorum");
-        final ILock lock = instance1.getLock("lockWithQuorum");
+//        final IQueue<String> queue = instance1.getQueue("queueWithQuorum");
+//        final ILock lock = instance1.getLock("lockWithQuorum");
 
-        // Quorum will succeed
-        System.out.println("Quorum is satisfied, so the following methods will not throw an exception");
-        queue.add("we have the quorum");
-
-        final String value = queue.poll();
-        System.out.println("Fetched '" + value + "' from the queue");
-
-        System.out.println("Lock operates as expected");
-        lock.lock();
-        System.out.println("The lock is locked : " + lock.isLocked());
-        lock.unlock();
+        final BlockingQueue<String> queue = instance1.getQueue("queueWithQuorum");
+        
+        try {
+            for (; ; ) {
+                System.out.println(queue.take());
+            }
+        } catch (HazelcastInstanceNotActiveException e) {
+            System.err.println("Unable to take from the queue. Hazelcast Member is probably going down!");
+        }
+        
+        
+//        // Quorum will succeed
+//        System.out.println("Quorum is satisfied, so the following methods will not throw an exception");
+//        queue.add("we have the quorum");
+//
+//        final String value = queue.poll();
+//        System.out.println("Fetched '" + value + "' from the queue");
+//
+//        System.out.println("Lock operates as expected");
+//        lock.lock();
+//        System.out.println("The lock is locked : " + lock.isLocked());
+//        lock.unlock();
 
         // Quorum will fail
 //        System.out.println("Shutdown one instance, so there won't be enough members for quorum presence");
